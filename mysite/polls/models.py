ddbb044745557b1b391a0b498bd3dfe960d89e4b
AhmedSalam22 <Ahmed0salam22@gmail.com>
2020-08-09 18:29:48 +0200

    C2A1

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,27 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length = 128)
-    email = models.CharField(max_length = 128)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
-        return "{}".format(self.name)
+        return self.question_text
+
+    
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
